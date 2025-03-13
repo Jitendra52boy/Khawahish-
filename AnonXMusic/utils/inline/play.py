f7f93817373ac2d7471a00a2d3732a38c94be167
Rudra <vbot34615@gmail.com>
2025-03-13 22:02:30 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -66,9 +66,6 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ]
-        [
-            )
-    ]
     ]
     
     return buttons
@@ -83,10 +80,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ]
-        [
-            )
-        ]
-    ]
+     ]
     return buttons
 
 
